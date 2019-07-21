cd41a1506e364f2e1ed53994342caf789997d6f3
alisha budhathoki <alishabudhathoki69@gmail.com>
2019-07-21 22:28:43 +0545

    initial commit

---
+++ app/src/main/java/com/example/roomrental/profile/ProfileActivity.java
@@ -0,0 +1,9 @@
+package com.example.roomrental.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+    private TextView mtextView;
+
+}
